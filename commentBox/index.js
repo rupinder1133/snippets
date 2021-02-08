d14dbe8bc7f79b3b9300c0a5d3f869db722efa50
Rupinder Singh <rupinder.singh@6sense.com>
2021-02-08 23:52:52 +0530

    add and render posts

--- commentBox/index.js
+++ commentBox/index.js
@@ -1,17 +1,70 @@
-const crud = {
+const postsCRUD = {
   storageKey: 'commentBox',
-  get posts() {
+  get read() {
     const storage = JSON.parse(localStorage.getItem(this.storageKey));
     if (storage === null) {
-      localStorage.setItem(this.storageKey, JSON.stringify({}));
+      this.update = [];
     }
-  }
+
+    return storage.posts;
+  },
+  set update(posts) {
+    localStorage.setItem(this.storageKey, JSON.stringify({ posts }));
+    renderPosts();
+  },
+  set create(post) {
+    const posts = this.read;
+    posts.push({ content: post });
+
+    this.update = posts;
+  },
 }
 
 function nuke() {
-  localStorage.setItem(crud.storageKey, JSON.stringify({}));
+  postsCRUD.update = [];
+  renderPosts();
 }
+
+function post() {
+  const postTextArea = document.getElementById('post-textarea');
+  if (!postTextArea) {
+    return;
+  }
+
+  postsCRUD.create = postTextArea.value;
+  postTextArea.value = '';
+}
+
+function renderPosts () {
+  function renderPost(parent, post) {
+    const postContainer = document.createElement('div');
+    postContainer.classList.add('postContainer');
+    postContainer.innerText = post.content;
+
+    parent.appendChild(postContainer);
+  }
+
+  let postsContainer = document.getElementById('posts');
+  if (!postsContainer) {
+    postsContainer = document.createElement('div')
+    postsContainer.id = 'posts';
+    document.body.appendChild(postsContainer);
+  } else {
+    while (postsContainer.firstChild) {
+      postsContainer.removeChild(postsContainer.firstChild);
+    }
+  }
+
+  const posts = postsCRUD.read;
+  posts.forEach((post) => {
+    renderPost(postsContainer, post);
+  });
+}
+
 
 function init() {
   document.getElementById('nuke').addEventListener('click', nuke);
+  document.getElementById('post-btn').addEventListener('click', post);
+
+  renderPosts();
 }
