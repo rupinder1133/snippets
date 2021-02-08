7e4cf3618eb1668aae1db8cc0114dde20fcee3f7
Rupinder Singh <rupinder.singh@6sense.com>
2021-02-09 00:22:33 +0530

    render basic replies

--- commentBox/index.js
+++ commentBox/index.js
@@ -14,7 +14,7 @@ const postsCRUD = {
   },
   set create(post) {
     const posts = this.read;
-    posts.push({ content: post });
+    posts.push({ content: post, replies: [] });
 
     this.update = posts;
   },
@@ -35,15 +35,51 @@ function post() {
   postTextArea.value = '';
 }
 
-function renderPosts () {
-  function renderPost(parent, post) {
-    const postContainer = document.createElement('div');
-    postContainer.classList.add('postContainer');
-    postContainer.innerText = post.content;
+function reply(postId) {
+  return function () {
+    const posts = postsCRUD.read;
 
-    parent.appendChild(postContainer);
+    postsCRUD.update = posts.map((post, id) => {
+      if (postId !== id) return post;
+
+      const replies = [...post.replies, { content: 'this is a comment on post' }]
+      return {...post, replies };
+    })
   }
+}
 
+function renderReplies(parent, replies) {
+  replies.forEach((reply) => {
+    const replyContainer = document.createElement('div');
+    replyContainer.innerText = reply.content;
+    parent.appendChild(replyContainer);
+  })
+}
+
+function renderPost(parent, post, id) {
+  const postContainer = document.createElement('div');
+  postContainer.classList.add('postContainer');
+
+  const postContent = document.createElement('div');
+  postContent.innerText = post.content;
+  postContent.classList.add('postContent');
+  postContainer.appendChild(postContent);
+
+  const replyBtn = document.createElement('button');
+  replyBtn.classList.add('replyBtn');
+  replyBtn.innerText = 'Reply';
+  replyBtn.addEventListener('click', reply(id));
+  postContainer.appendChild(replyBtn);
+
+  const repliesContainer = document.createElement('div');
+  repliesContainer.classList.add('replies');
+  renderReplies(repliesContainer, post.replies);
+  postContainer.appendChild(repliesContainer);
+
+  parent.appendChild(postContainer);
+}
+
+function renderPosts () {
   let postsContainer = document.getElementById('posts');
   if (!postsContainer) {
     postsContainer = document.createElement('div')
@@ -56,8 +92,8 @@ function renderPosts () {
   }
 
   const posts = postsCRUD.read;
-  posts.forEach((post) => {
-    renderPost(postsContainer, post);
+  posts.forEach((post, index) => {
+    renderPost(postsContainer, post, index);
   });
 }
 
